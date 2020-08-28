5d0f6c84212cc643a2154233a713888c0cc622a0
YuWu-Seymour <ywuu0108@student.monash.edu>
2020-08-28 17:17:35 +1000

    return html

---
+++ src/main/java/oceanusproject/demov1/controller/ContentPageController.java
@@ -0,0 +1,10 @@
+package oceanusproject.demov1.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/content")
+public class ContentPageController {
+
+}
---
+++ src/main/java/oceanusproject/demov1/controller/HomePageController.java
@@ -0,0 +1,13 @@
+package oceanusproject.demov1.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+@Controller
+public class HomePageController {
+    @GetMapping
+    public String getHomePage() {
+        return "index";
+    }
+}
