0b773d77b3ba4171b4fcdb43511d8f964a87ab1b
Malcolm Malloy <malcolm.malloy@gmail.com>
2020-09-03 01:07:02 +1000

    Completing last push

--- src/main/resources/static/js/template.js
+++ src/main/resources/static/js/template.js
@@ -52,7 +52,6 @@ function buildHeader() {
     <div class="nav-option-group">
       <ul>
         <li class="nav-option"><a href="` + navHomeURL + `">Home</a></li>
-        <li class="nav-option"><a href="` + navAboutURL + `">About</a></li>
         <li class="nav-option"><a href="` + navMapURL + `">Explore</a></li>
         <li class="nav-option"><a href="` + navContentURL + `">Quiz</a></li>
       </ul>
